83156380c7458f9ec1e5291646dba07df32e91f8
JHY3066 <jkjjjfamily30@gmail.com>
2023-11-25 02:36:07 +0900

    지도랑 게시판 이동 버튼

--- app/src/main/java/com/example/kliv/MainActivity.kt
+++ app/src/main/java/com/example/kliv/MainActivity.kt
@@ -3,16 +3,22 @@ package com.example.kliv
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.example.kliv.databinding.ActivityMainBinding
 import com.example.kliv.databinding.FragmentBoardBinding
+import com.example.kliv.ui_button.HomebtnActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        val binding = FragmentBoardBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        Handler().postDelayed(Runnable {
+            val i = Intent(this, HomebtnActivity::class.java)
+            startActivity(i)
 
-        startActivity(Intent(this, BoardActivity::class.java))
+            finish()
+        }, 3000)
+    }
     }
 }
