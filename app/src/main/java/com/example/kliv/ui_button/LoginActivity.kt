e0dabd3cf74eae9c82c77a4c0a784690297bdde6
sunflow-er <antjd03170@gmail.com>
2023-11-25 04:05:39 +0900

    login & sign_in click event

--- app/src/main/java/com/example/kliv/ui_button/LoginActivity.kt
+++ app/src/main/java/com/example/kliv/ui_button/LoginActivity.kt
@@ -1,14 +1,25 @@
 package com.example.kliv.ui_button
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.kliv.MainActivity
+import com.example.kliv.MapActivity
 import com.example.kliv.R
+import com.example.kliv.databinding.ActivityLoginBinding
 import com.google.android.gms.maps.SupportMapFragment
 
 public class LoginActivity : AppCompatActivity() {
+    lateinit var binding : ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // setContentView()
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginBtn.setOnClickListener {
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+        }
 
 
     }
--- app/src/main/java/com/example/kliv/ui_button/PerInfoActivity.kt
+++ app/src/main/java/com/example/kliv/ui_button/PerInfoActivity.kt
@@ -1,15 +1,25 @@
 package com.example.kliv.ui_button
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
+import com.example.kliv.MainActivity
 import com.example.kliv.R
+import com.example.kliv.databinding.ActivityLoginBinding
 import com.google.android.gms.maps.SupportMapFragment
 
 public class PerInfoActivity : AppCompatActivity() {
+    lateinit var binding : ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView()
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.loginBtn.setOnClickListener {
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+        }
     }
-
 }
